56fdc673eb19289fb96f924bbfbdc2911b6125b0
Neil Babson <nbabson@pdx.edu>
2018-11-25 00:50:33 -0800

    upload cover picture

--- crochet-gallery.js
+++ crochet-gallery.js
@@ -11,7 +11,9 @@ server.get('/crochet-gallery.html', function(req, res) {
 			'Content-Type': 'text/html'
 		});
                 res.write(mustache.render(data.toString(), {
-                    'testdata': [{'test': 'hello', 'picture': 'crochet.jpg'} ] }));
+                    'testdata': [{'test': 'hello',
+                    'picture': 'https://raw.githubusercontent.com/nbabson/crochet-gallery/master/crochet.png' }] 
+                }));
                 res.end();
         });
 });
