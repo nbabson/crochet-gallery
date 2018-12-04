5613fb082af9b7a4411dc1fe856a560cdd8729d6
Neil Babson <nbabson@pdx.edu>
2018-12-03 21:06:19 -0800

    moved music files

--- crochet-gallery.js
+++ crochet-gallery.js
@@ -58,7 +58,7 @@ server.get('/menu.html', function(req, res) {
                         'Content-Type': 'text/html'
                 });
                 res.write(mustache.render(data.toString(), {
-                     "menupage": [{ "music": 'http://www.blockmrecords.org/bach/audio/mp3/BWV0573.mp3'}]
+                     "menupage": [{ "music": 'http://raw.githubusercontent.com/nbabson/crochet-gallery/master/music/BWV0573.m4a}]
                 }));
                 res.end();
         });
@@ -197,7 +197,7 @@ server.get('/contact.html', function(req, res) {
                 });
                 res.write(mustache.render(data.toString(), {
                      'contactpage': [{'neil': 'https://raw.githubusercontent.com/nbabson/crochet-gallery/master/images/storm3.png',
-                     'kibbie': 'http://www.blockmrecords.org/images/artists/Kibbie.jpg' }]      
+                     'kibbie': 'https://raw.githubusercontent.com/nbabson/crochet-gallery/master/images/Kibbie.jpg' }]      
                 }));
                 res.end();
         });
